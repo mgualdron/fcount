ecc8d1e8033abd33e54e56b3856f6546ba33a0f9
Miguel Gualdron <dev@gualdron.com>
2016-04-25 11:47:20 -0500

    New --help and support for input from STDIN.

--- src/fcount.c
+++ src/fcount.c
@@ -4,10 +4,14 @@
 // Purpose:         To count the number of fields (columns) in a delimted
 //                  text file.
 //
-// Notes:           Use a struct to store the field-count and file name,
-//                  along with the frequency for each field-count.  Then with
-//                  an array of structs, record all values observed, and
+// Notes:           Use a struct to store the field-count along with the
+//                  frequency for each field-count.  Then with a dynamic
+//                  array of structs, record all values observed, and
 //                  report them at the end.
+//
+//                  The dynamic array implementation comes from "Learn C The
+//                  Hard Way" by Zed Shaw, who also implemented the debug and
+//                  unit testing macros.
 // -------------------------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,9 +21,7 @@
 #include <lcthw/dbg.h>
 #include "fcount.h"
 
-
-static int verbose_flag;
-
+static const char* program_name = "fcount";
 
 // A function to create FCount struct instances:
 struct FCount *FC_create (char *filename, int fieldcount, int recordcount) {
@@ -74,6 +76,7 @@ void FC_print(FCount *fc)
     // Did we get a valid pointer passed in?
     assert(fc != NULL);
 
+    // Print it:
     printf("%d\t%d\t%s\n", fc->fieldcount, fc->recordcount, fc->filename);
 }
 
@@ -118,27 +121,63 @@ error:
     return -1;
 }
 
+static void try_help () {
+    printf("Try '%s -h' for more information.", program_name);
+    exit(1);
+}
+
+
+static void usage (int status) {
+    if (status != 0) {
+        try_help();
+    }
+    else {
+      printf ("\
+Usage: %s [OPTION]... [FILE]...\n\
+", program_name);
+
+      fputs ("\
+Print field (column) count and number of lines for each count.\n\
+More than one FILE can be specified.\n\
+", stdout);
+
+      fputs ("\
+\n\
+  -d, --delimiter        the delimiting character for the input FILE(s)\n\
+  -H, --header           print a header line with the output counts\n\
+  -q, --quiet            do not output counts, but return 2 if\n\
+                         multiple field counts are detected\n\
+                         i.e. the file is inconsistent.\n\
+", stdout);
+    }
+
+    exit (status);
+}
+
+
+static struct option long_options[] = {
+    {"quiet",     no_argument,       0, 'q'},
+    {"header",    no_argument,       0, 'H'},
+    {"delimiter", required_argument, 0, 'd'},
+    {"help",      no_argument,       0, 'h'},
+    {0, 0, 0, 0}
+};
+
 
 int main (int argc, char *argv[])
 {
     int c;
     char *delim = "\t";
+    int be_quiet = 0;
+    int show_header = 0;
+    int inconsistent_file = 0;
 
     while (1) {
-        static struct option long_options[] = {
-            // These options set a flag.
-            {"verbose", no_argument,       &verbose_flag, 1},
-            {"brief",   no_argument,       &verbose_flag, 0},
-            // These options don't set a flag.
-            {"delimiter", required_argument, 0, 'd'},
-            {"lines",     no_argument,       0, 'l'},
-            {0, 0, 0, 0}
-        };
 
         // getopt_long stores the option index here.
         int option_index = 0;
 
-        c = getopt_long (argc, argv, "ld:", long_options, &option_index);
+        c = getopt_long (argc, argv, "hHqd:", long_options, &option_index);
 
         // Detect the end of the options.
         if (c == -1) break;
@@ -146,25 +185,26 @@ int main (int argc, char *argv[])
         switch (c) {
             case 0:
                 // If this option set a flag, do nothing else now.
-                if (long_options[option_index].flag != 0) {
-                  break;
-                }
-
-                debug("option %s", long_options[option_index].name);
-                if (optarg) {
-                  debug(" ...with arg %s", optarg);
-                }
-                //printf("\n");
-
-                break;
-
-            case 'l':
-                debug("option -l");
                 break;
 
             case 'd':
-                debug("option -d with value `%s'\n", optarg);
+                debug("option -d with value `%s'", optarg);
                 delim = optarg;
+                break;
+
+            case 'h':
+                debug("option -h");
+                usage(0);
+                break;
+
+            case 'H':
+                debug("option -H");
+                show_header = 1;
+                break;
+
+            case 'q':
+                debug("option -q");
+                be_quiet = 1;
                 break;
 
             // getopt_long already printed an error message.
@@ -172,30 +212,43 @@ int main (int argc, char *argv[])
                 break;
 
             default:
-                abort ();
+                usage(1);
         }
     }
 
-    // Report the final status resulting from "--verbose" or "--brief"
-    if (verbose_flag) {
-        debug("verbose flag is set");
+    if (show_header && !be_quiet) {
+        printf("field_count\trecords\tfile\n");
     }
-  
-    // Process any remaining command line arguments (input files).
-    if (optind < argc) {
-        while (optind < argc) {
 
-            char *filename = argv[optind];
+    int j = optind;  // A copy of optind (the number of options at the command-line)
+
+    // Process any remaining command line arguments (input files).
+    if (j <= argc) {
+
+        do {
+
+            char *filename = argv[j];
             char *line = NULL;
             size_t len = 0;   // allocated size for line
             ssize_t read = 0; // num of chars read
-            char *token;
+            char *token = NULL;
             int fieldcount = 0;
             DArray *darray = DArray_create(sizeof(FCount), 10);
 
-            debug("filename = %s", filename);
 
-            FILE *fp = fopen(filename , "r");
+            if (optind == argc) {
+                filename = "-";
+            }
+
+            FILE *fp = NULL;
+
+            if (filename[0] == '-') {
+                fp = stdin;
+            }
+            else {
+                fp = fopen(filename , "r");
+            }
+
             check(fp != NULL, "Error opening file: %s.", filename);
 
             while ((read = getline(&line, &len, fp)) != -1) {
@@ -204,19 +257,31 @@ int main (int argc, char *argv[])
                     fieldcount++;
                 }
                 check(FC_array_push(darray, filename, fieldcount) == 0, "Error pushing element into darray.");
+                if (darray->end > 1) { // If we have more than one field count in this file
+                    inconsistent_file = 2;
+                }
             }
 
             free(line);
             fclose(fp);
 
-            FC_array_print(darray);
+            if (!be_quiet) {
+                FC_array_print(darray);
+            }
             FC_array_destroy(darray);
 
-            optind++;
-        }
+            j++;
+
+        } while (j < argc);
+
     }
   
-    return 0;
+    if (be_quiet) {
+        return inconsistent_file;
+    }
+    else {
+        return 0;
+    }
 
 error:
     return -1;
